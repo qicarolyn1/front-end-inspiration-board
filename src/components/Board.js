bb8ab8d2831a9c2b1d5d0f9935574d2733870c1a
Aiyana Ball <ballaiyana@gmail.com>
2023-06-27 16:17:01 -0700

    changed return of boards

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Board = (props) => {
-  // handleDeleteClick will go here
 
+const Board = ({ board, onDelete }) => {
   return (
-    <div className="board">
-      <h2>{props.board.title}</h2>
-      <p>Owner: {props.board.owner}</p>
-      <p>Description: {props.board.description}</p>
-      <div className="board-controls">
-        {/* <button className="board-delete-button" onClick={handleDeleteClick}> */}
-          {/* Delete */}
-        {/* </button> */}
-      </div>
+    <div>
+      <h2>{board.title}</h2>
+      <p>Owner: {board.owner}</p>
+      <button onClick={() => onDelete(board.id)}>Delete</button>
     </div>
   );
 };
