bf62dbdfc26336d18a625d685cf067e6575553c8
Aiyana Ball <ballaiyana@gmail.com>
2023-06-27 16:23:36 -0700

    Created NewBoardForm function

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+
+const NewBoardForm = ({ onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" name="title" required />
+      <br />
+      <label htmlFor="owner">Owner</label>
+      <input type="text" id="owner" name="owner" required />
+      <br />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default NewBoardForm;
