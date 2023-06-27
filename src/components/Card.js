fd41f1edf7c648d4f3a93eeb725ab2a2c98433e5
Carolyn Qi <qi.carolyn@gmail.com>
2023-06-26 23:31:42 -0700

    Add Card and Card List code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,27 @@
 import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+
+const Card = (props) => {
+    
+
+    return (
+        <li className="card_item">
+            <h3>{props.message}</h3>
+            <ul class="card_item_controls">
+                <li>
+                    <p>{props.likes}💕</p>
+                </li>
+                <li>
+                    <button className="card_item_like_button">"+1"</button>
+                </li>
+                <li>
+                    <button className="card_item_remove_button">"Delete"</button>
+                </li>
+            </ul>
+        </li>
+    );
+};
+
+export default Card
+
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,3 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import Card from './Card.js';
+
+const CardList = ({ cards }) => {
+    const getCardListJSX = (cards) => {
+        return cards.map((cards) => {
+            return (
+                <CARD 
+                key={cards.id}
+                id={cards.id}
+                message={cards.message}
+                likes_count={cards.likes_count}
+                />
+            );
+        });
+    };
+    return <ul className="card_list">{getCardListJSX(cards)}</ul>;
+};
+
+CardList.propTypes = {
+    cards: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            message: PropTypes.string.isRequired,
+            likes_count: PropTypes.number.isRequired,
+        })
+    ).isRequired,
+};
+
+export default CardList;
