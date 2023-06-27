9635396526b925b8f893173d21a269021aab4a2d
Aiyana Ball <ballaiyana@gmail.com>
2023-06-27 16:14:33 -0700

    Moved board list and new board form; added create/delete board functions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ import CardList from './components/CardList.js'
 
 import BoardList from './components/BoardList';
 import Board from './components/Board';
+import NewBoardForm from './components/NewBoardForm';
 
+const BOARDS = [
+  { id: 1, title: 'Banana', owner: 'Anh', cards: [] },
+  { id: 2, title: 'Board 2', owner: 'Kim', cards: [] },
+  { id: 3, title: 'Board 3', owner: 'Carolyn', cards: [] },
+];
 
 function App() {
   const CARDS = [
@@ -39,24 +45,7 @@ function App() {
     }
   ];
 
-
-  const BOARDS = [
-    {
-      id: 1,
-      title: 'Travel',
-      description: 'Places to visit',
-    },
-    {
-      id: 2,
-      title: 'Cats',
-      description: 'Good Kitties',
-    }
-  ];
-
   const [cards, setCards] = useState(CARDS)
-
-  const [boards, setBoards] = useState([]);
-  const [selectedBoardId, setSelectedBoardId] = useState(null)
 
   const increaseLikes = (id, originalLike) => {
     const newLike = originalLike + 1
@@ -80,45 +69,52 @@ function App() {
     setCards(newCards);
   };
 
-  useEffect(() => {
-    axios.get('http://localhost:3000/boards')
-      .then(response => {
-        setBoards(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
 
-  const handleBoardSelect = (id) => {
-    setSelectedBoardId(id);
+  const [boards, setBoards] = useState(BOARDS);
+  const [selectedBoard, setSelectedBoard] = useState(null);
+
+
+  function handleSelectBoard(boardId) {
+    const selected = boards.find(board => board.id === boardId);
+    setSelectedBoard(selected);
+  }
+
+  const deleteBoard = (boardId) => {
+    setBoards((prevBoards) => prevBoards.filter((board) => board.id !== boardId));
+    setSelectedBoard(null);
   };
 
-  const selectedBoard = boards.find(board => board.id === selectedBoardId);
-  
+  function createBoard(title, owner) {
+    const newBoard = { id: Date.now(), title, owner };
+    setBoards(prevBoards => [...prevBoards, newBoard]);
+  }
+
+
   return (
     
     <div className="App">
       <header className="App-header">
         <h1>Inspiration Board</h1>
       </header>
-      <main>
-        <div className="boards">
-          <h2>Boards</h2>
-          <BoardList boards={BOARDS} onSelect={handleBoardSelect} />
-        </div>
-        <div className="board-details">
-          {selectedBoardId ? (
-            <p>Selected board id: {selectedBoardId}</p>
-          ) : (
-            <p>Select a board to view cards</p>
-          )}
-        </div>
-      </main>
       <main className="container">
         <div className="forms_content">
-          <p>Form1</p>
-          <p>Form2</p>
+          <div className="board_list">
+            <h2>Boards</h2>
+            {!selectedBoard && <p>Please select a board from the list below.</p>}
+        {selectedBoard && (
+          <div>
+            <h2>
+              {selectedBoard.title} ({selectedBoard.owner})
+            </h2>
+            <Board board={selectedBoard} onDelete={deleteBoard} />
+          </div>
+        )}
+        <BoardList boards={BOARDS} onSelect={handleSelectBoard} />
+          </div>
+        <div className="new_board">
+          <h2>Create New Board</h2>
+            <NewBoardForm onCreate={createBoard} />
+        </div>
           <p>Form3</p>
         </div>
         <div className="cards_content">
