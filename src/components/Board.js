dbd03185417f57a759b975855c05d44a62a42cf3
Aiyana Ball <ballaiyana@gmail.com>
2023-06-27 13:01:53 -0700

    Added Board and Board List

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,22 @@
 import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+
+const Board = (props) => {
+  // handleDeleteClick will go here
+
+  return (
+    <div className="board">
+      <h2>{props.board.title}</h2>
+      <p>Owner: {props.board.owner}</p>
+      <p>Description: {props.board.description}</p>
+      <div className="board-controls">
+        {/* <button className="board-delete-button" onClick={handleDeleteClick}> */}
+          {/* Delete */}
+        {/* </button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import Board from './Board.js';
+
+const BoardList = ({ boards, onSelect }) => {
+  const getBoardListJSX = (boards) => {
+    return boards.map((board) => {
+      return (
+        <Board
+          key={board.id}
+          board={board}
+          onDelete={() => alert('Delete Board')}
+          onSelect={() => onSelect(board.id)}
+        />
+      );
+    });
+  };
+
+  return <ul className="board-list">{getBoardListJSX(boards)}</ul>;
+};
+
+export default BoardList;
