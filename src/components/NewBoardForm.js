65aa4bc99a997dbf787bc5e8cb5f0c3f7a0858a5
Aiyana Ball <ballaiyana@gmail.com>
2023-06-28 11:12:43 -0700

    Added ability to hide new board form

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-const NewBoardForm = ({ onSubmit }) => {
+const NewBoardForm = ({ onSubmit, onHide }) => {
+  const [showForm, setShowForm] = useState(true);
   const handleSubmit = (event) => {
     event.preventDefault();
     const title = event.target.title.value;
     const owner = event.target.owner.value;
     onSubmit(title, owner);
+    setShowForm(false);
+    event.target.reset();
   };
+
+  function handleHide() {
+    setShowForm(false);
+    onHide();
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Title</label>
-      <input type="text" id="title" name="title" required />
-      <br />
-      <label htmlFor="owner">Owner</label>
-      <input type="text" id="owner" name="owner" required />
-      <br />
-      <button type="submit">Add</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" required />
+        <br />
+        <label htmlFor="owner">Owner</label>
+        <input type="text" id="owner" name="owner" required />
+        <br />
+        <button type="submit">Submit New Board</button>
+        <br />
+        <button type="button" onClick={handleHide}>Hide Form</button>
+      </form>
+    </div>
   );
 };
 
