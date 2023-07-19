83007af45084490438a97d37ffa00457b54dd3e2
Aiyana Ball <ballaiyana@gmail.com>
2023-07-18 23:53:10 -0700

    set new board form spacing

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -7,8 +7,8 @@ const NewBoardForm = ({ onSubmit, onHide }) => {
     const title = event.target.title.value;
     const owner = event.target.owner.value;
     onSubmit(title, owner);
-    setShowForm(false);
     event.target.reset();
+    handleHide();
   };
 
   function handleHide() {
@@ -18,19 +18,22 @@ const NewBoardForm = ({ onSubmit, onHide }) => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" name="title" required />
-        <br />
-        <label htmlFor="owner">Owner</label>
-        <input type="text" id="owner" name="owner" required />
-        <br />
-        <button type="Submit">Submit New Board</button>
-        <br />
-        <button type="button" onClick={handleHide}>Hide Form</button>
+      <form onSubmit={handleSubmit} className="new-board-form">
+        <div className="form-group">
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" name="title" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="owner">Owner:</label>
+          <input type="text" id="owner" name="owner" required />
+        </div>
+        <div className="button-group">
+          <button type="Submit" className="form-button">Submit New Board</button>
+          <button type="Hide" className="form-button" onClick={handleHide}>Hide Form</button>
+        </div>
       </form>
     </div>
   );
-};
+}
 
 export default NewBoardForm;
